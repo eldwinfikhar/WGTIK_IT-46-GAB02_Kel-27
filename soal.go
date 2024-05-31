0a51c5e8e35d1166a27e8efd0c9c802c8326191e
vinctven <vinct.ven@gmail.com>
2024-05-31 17:11:12 +0700

    UPDATE soal.go

--- soal.go
+++ soal.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func platNo(tanggal int, plat string) bool {
-	var x string
+	var found, x byte
 	var ganjil, genap, hasil bool
-	x = plat[4:5]
-	ganjil = x == "1" || x == "3" || x == "5" || x == "7" || x == "9"
-	genap = x == "2" || x == "4" || x == "6" || x == "8" || x == "0"
+	var i int
+
+	i = 1
+	for {
+		found = plat[len(plat)-i]
+		i++
+		if found == '_' {
+			break
+		}
+	}
+	x = plat[len(plat)-i]
+	ganjil = x == '1' || x == '2' || x == '5' || x == '7' || x == '9'
+	genap = x == '2' || x == '4' || x == '6' || x == '8' || x == '0'
 	if tanggal%2 != 0 && ganjil {
 		hasil = true
 	} else if tanggal%2 == 0 && genap {
